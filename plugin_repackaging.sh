470ec0f4da88d18af435019a4d5f076c80962a3e
zzzz666-zz <75197442+zzzz666-zz@users.noreply.github.com>
2026-02-11 09:57:57 +0800

    Update pandas and PyMuPDF version constraints

--- plugin_repackaging.sh
+++ plugin_repackaging.sh
@@ -113,6 +113,7 @@ repackage(){
 	cd ${CURR_DIR}/${PACKAGE_NAME}
 	echo "正在强制修正 pandas 版本依赖..."
 	find . -name "requirements.txt" -exec sed -i 's/pandas\[.*\]~=2.3.3/pandas[excel,html,xml]>=2.3.2/g' {} +
+	find . -name "requirements.txt" -exec sed -i 's/PyMuPDF~=1.26.4/PyMuPDF>=1.26.0/g' {} +
 	echo "修正完成，继续执行..."
 	pip download ${PIP_PLATFORM} -r requirements.txt -d ./wheels --index-url ${PIP_MIRROR_URL} --trusted-host mirrors.aliyun.com
 	if [[ $? -ne 0 ]]; then
